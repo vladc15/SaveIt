f1894d53f5f7a72356a8d2ddd941fecab2301470
vladc15 <vciuperceanu@gmail.com>
2023-12-15 22:52:33 +0200

    integrated select2 for user-friendly multiple select

--- SaveIt/wwwroot/js/site.js
+++ SaveIt/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$('#multiple-select-field').select2({
+    theme: "bootstrap-5",
+    width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
+    placeholder: $(this).data('placeholder'),
+    closeOnSelect: false,
+});
